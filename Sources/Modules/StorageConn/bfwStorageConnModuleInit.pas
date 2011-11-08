b60ad0495a73c6082775892af393c68fc785a1ae
serg <owner.bsf@gmail.com>
2011-11-08 13:50:40 +0200

    move StorageConn

---
+++ Sources/Modules/StorageConn/bfwStorageConnModuleInit.pas
@@ -0,0 +1,33 @@
+unit bfwStorageConnModuleInit;
+
+interface
+
+uses classes, CoreClasses, StorageConnIBX, EntityServiceIntf;
+
+type
+  TbfwStorageConnModuleInit = class(TModule)
+  public
+    class function Kind: TModuleKind; override;
+    procedure Load; override;
+  end;
+
+implementation
+
+{ TbfwtorageConnIBXModuleInit }
+
+class function TbfwStorageConnModuleInit.Kind: TModuleKind;
+begin
+  Result := mkInfrastructure;
+end;
+
+procedure TbfwStorageConnModuleInit.Load;
+begin
+  (WorkItem.Services[IEntityManagerService] as IEntityManagerService).
+    Connections.
+      RegisterConnectionFactory(TIBStorageConnectionFactory.Create(Self));
+end;
+
+initialization
+  RegisterModule(TbfwStorageConnModuleInit);
+
+end.
